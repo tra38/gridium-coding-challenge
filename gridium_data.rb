9befd0c927b875fec57a7d0c877806969d2b8b20
tra38 <tra38@nau.edu>
2018-02-02 17:18:46 -0600

    Display table with proper data

--- gridium_data.rb
+++ gridium_data.rb
@@ -1,16 +1,42 @@
 require 'net/http'
+require 'json'
+require 'pry'
 
 class GridiumData
-  attr_reader :url
+  attr_reader :url, :rows
   def initialize(url)
     @url = URI.parse(url)
+    json = JSON.parse(get_json)
+    @rows = json["data"].map do |row|
+      GridiumRow.new(row)
+    end
+    binding.pry
   end
 
+  private
   def get_json
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port, :use_ssl => true) {|http|
       http.request(req)
     }
     res.body
+  end
+end
+
+class GridiumRow
+  attr_reader :id, :meter_id, :building_id, :used_energy, :peak_energy, :closing_date, :initial_date, :cost
+  def initialize(json)
+    @id = json["id"]
+    @used_energy = json["attributes"]["used"]
+    @peak_energy = json["attributes"]["peak"]
+    @closing_date = json["attributes"]["closing"]
+    @initial_date = json["attributes"]["initial"]
+    @cost = number_to_currency(json["attributes"]["cost"])
+  end
+
+  private
+  # https://stackoverflow.com/questions/35561553/format-string-as-currency-with-ruby-sinatra
+  def number_to_currency(number)
+    "$#{number}"
   end
 end
--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -6,11 +6,14 @@
 # $ ruby heroku-sinatra-app.rb
 #
 require 'sinatra'
+require 'json'
 require_relative 'gridium_data'
+require 'pry'
 
 # Quick test
 get '/' do
   data = GridiumData.new("https://snapmeter.com/api/v2/531e19848df5cb0b35014e85/meters/2166484536790/bill-summary?token=6d547442-417b-41a3-8838-b022f9c2974d")
-  @json = data.get_json
+  binding.pry
+  @array_of_bills = data.rows
   erb :index
 end
