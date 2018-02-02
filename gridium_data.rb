0eb23e68a964eab369802d1789fdde0d1321f23f
tra38 <tra38@nau.edu>
2018-02-02 16:15:52 -0600

    Grab JSON from external server and display it to the user

---
+++ gridium_data.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+
+class GridiumData
+  attr_reader :url
+  def initialize(url)
+    @url = URI.parse(url)
+  end
+
+  def get_json
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body
+  end
+end
--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -6,8 +6,11 @@
 # $ ruby heroku-sinatra-app.rb
 #
 require 'sinatra'
+require_relative 'gridium_data'
 
 # Quick test
 get '/' do
+  data = GridiumData.new("https://snapmeter.com/api/v2/531e19848df5cb0b35014e85/meters/2166484536790/bill-summary?token=6d547442-417b-41a3-8838-b022f9c2974d")
+  @json = data.get_json
   erb :index
 end
